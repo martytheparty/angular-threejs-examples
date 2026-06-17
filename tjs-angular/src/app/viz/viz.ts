441220e1f8e6dc09b5ef64d3ca2668b4b5d988fc
Marty Trujillo <marty@ilikeemail.com>
2026-06-17 15:01:05 -0400

    Removes code that does not apply to this viz.

--- tjs-angular/src/app/viz/viz.ts
+++ tjs-angular/src/app/viz/viz.ts
@@ -2,7 +2,6 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
-  effect,
   inject,
   ViewChild
 } from '@angular/core';
@@ -23,36 +22,16 @@ import { VizAnimation } from './viz.animation.class';
   styleUrl: './viz.scss',
 })
 export class VizComponent implements AfterViewInit {
-
   @ViewChild('visualization', { static: true })
   visualization!: ElementRef<HTMLDivElement>;
   controlsService: ControlsService = inject(ControlsService);
 
-  segmentCount = 3; // triangle
   scene = new THREE.Scene();
   group: THREE.Object3D = new THREE.Group();
   renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer( { antialias: true } );
   camera: THREE.PerspectiveCamera | undefined ;
-  initialized = false;
-
-  constructor() {
-    effect( () => {
-      this.segmentCount = this.controlsService.s();
-      if (this.initialized) {
-        this.removeGroupFromScene(this.group, this.scene);
-        // get the new group
-        this.group = this.createGroup();
-        this.addGroupToScene(this.group, this.scene);
-        if (this.camera) {
-          this.setAnimation(this.renderer, this.camera )
-        }
-
-      }
-    } );
-  }
 
   ngAfterViewInit(): void {
-    this.initialized = true;
     const width = window.innerWidth, height = window.innerHeight;
     this.camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 20 );
     
@@ -83,7 +62,6 @@ export class VizComponent implements AfterViewInit {
           renderer.render(this.scene, camera);
         }
      );
-
   }
 
   createGroup(): THREE.Object3D {
@@ -109,40 +87,13 @@ export class VizComponent implements AfterViewInit {
     const frontMesh = new THREE.Mesh(geometry, frontMaterial);
     const backMesh = new THREE.Mesh(geometry, backMaterial);
 
-    //scene.add(group);
     this.group = new THREE.Group();
-
-    this.group.add(frontMesh);
-    this.group.add(backMesh);
+    this.group.add(tetrahedron);
 
     return this.group;
   }
 
   addGroupToScene( group: THREE.Object3D, scene: THREE.Scene): void {
     scene.add(group);
-  }
-
-  removeGroupFromScene(group: THREE.Object3D, scene: THREE.Scene): void {
-
-    scene.remove(group);
-
-    group.traverse((child: any) => {
-
-      if (child.geometry) {
-        child.geometry.dispose();
-      }
-
-      if (child.material) {
-
-        if (Array.isArray(child.material)) {
-          child.material.forEach((m: any) => m.dispose());
-        } else {
-          child.material.dispose();
-        }
-
-      }
-
-    });
-
   }
 }
